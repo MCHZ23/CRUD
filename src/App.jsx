450bdc56a92bc320dc43e1b5380fbd529f2ce96e
Omar Martinez <107728025+MCHZ23@users.noreply.github.com>
2023-08-26 20:59:01 -0600

    whats done so far

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,37 @@
 
+import { useEffect } from 'react'
 import './App.css'
+import useFetch from './hooks/useFetch'
+import UserCard from './components/UserCard'
+
 
 function App() {
+
+
+  const baseUrl = 'https://users-crud.academlo.tech'
+  const [ 
+    users,
+    getAllUsers,
+    createNewUser,
+    delectUserById,
+    updateUserById 
+  ] = useFetch(baseUrl)
+
+  useEffect(() =>  { 
+    getAllUsers('/users')
+  }, [])
+
 
   return (
     <div className='app'>
       <h1>semana 4</h1>
+      <div>
+      {
+        users?.map((user) => (
+          <UserCard key={user.id} user={user}/>
+        )) 
+      }
+      </div>
     </div>
   )
 }
